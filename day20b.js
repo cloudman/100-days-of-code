66e4110320cfb9ef9a43265e6c96860a30800405
cloudman <cloudman@mac.com>
2019-03-12 18:25:48 -0500

    Day 20 #100DaysOfCode using arrow function syntax with higher order function including map and filter.

---
+++ day20b.js
@@ -0,0 +1,11 @@
+index.js const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter((item) => item > 0 && item === Math.floor(item)).map((item) => item * item);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
