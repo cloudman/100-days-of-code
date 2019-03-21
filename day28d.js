2d2aee592410bf30ef9cbb59268f9e0e534fff65
cloudman <cloudman@mac.com>
2019-03-20 23:41:43 -0500

    day28 #100DaysOfCode Slice and Splice to make a FrankenSplice Basic Algorithm Scripting FreeCodeCamp

---
+++ day28d.js
@@ -0,0 +1,11 @@
+index.js function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice(0, arr2.length);
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++
+  }
+  return localArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
