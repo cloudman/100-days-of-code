6e2be299c363e0fa6b56d955617b52c822fb959f
cloudman <cloudman@mac.com>
2019-03-24 14:46:06 -0500

    Day33 #100DaysOfCode Refactor Global Variables Out Of Functions Functional Programming FreeCodeCamp

---
+++ day33a.js
@@ -0,0 +1,35 @@
+index.js // the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (arr, bookName) {
+  let theBookList = [...arr];
+  theBookList.push(bookName);
+  return theBookList;
+  
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (arr, bookName) {
+  let aBookList = [...arr];
+  if (aBookList.indexOf(bookName) >= 0) {
+    let index = aBookList.indexOf(bookName);
+    aBookList.splice(index, 1);
+    return aBookList;
+    
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
