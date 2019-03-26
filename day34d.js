8339ed4296362a5a6bf19c60aba74fd98f9b67c2
cloudman <cloudman@mac.com>
2019-03-25 22:42:17 -0500

    Day34 #100DaysOfCode Add Elements to the end of an Array using concat instead of push method
    Functional Programming section of FreeCodeCamp

---
+++ day34d.js
@@ -0,0 +1,9 @@
+index.js function nonMutatingPush(original, newItem) {
+  // Add your code below this line
+  let start = [];
+  return original.concat(start.concat(newItem));
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
