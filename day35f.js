ca3a86ba89e0c53b7a2643ceceec264f4baaed7c
cloudman <cloudman@mac.com>
2019-03-26 21:47:26 -0500

    Day35 #100DaysOfCode Use the some method to check that any element in an Array meets a criteria
    Functional Programming section of FreeCodeCamp

---
+++ day35f.js
@@ -0,0 +1,7 @@
+index.js function checkPositive(arr) {
+  // Add your code below this line
+  return arr.some(element => element > 0);
+  
+  // Add your code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
