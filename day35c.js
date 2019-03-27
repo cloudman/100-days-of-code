35d49c331cb8f466ba242bdd38bd98d8322776af
cloudman <cloudman@mac.com>
2019-03-26 20:59:51 -0500

    Day35 #100DaysOfCode Combine an Array into a String using the join method.
    Functional Programming section of FreeCodeCamp

---
+++ day35c.js
@@ -0,0 +1,7 @@
+index.js function sentensify(str) {
+  // Add your code below this line
+  return str.split(/\W/).join(" ");
+  
+  // Add your code above this line
+}
+sentensify("May-the-force-be-with-you");
