1a92ee967cd33aec282d5bb4c218ded1f069f4a3
cloudman <cloudman@mac.com>
2019-03-30 02:50:51 -0500

    day39 #100DaysOfCode whatIsInAName
    Intermediate Algorithm Scripting section of FreeCodeCamp

---
+++ day39a.js
@@ -0,0 +1,20 @@
+index.js function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+
+  // Only change code below this line
+  var srcKeys = Object.keys(source);
+  arr = collection.filter(function(obj) {
+    for (let i = 0; i < srcKeys.length; i++) {
+      if (!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
