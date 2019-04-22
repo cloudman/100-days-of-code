095d3cc40f785aa3cc501b9708644dbbda94026e
cloudman <cloudman@mac.com>
2019-04-21 21:51:52 -0500

    day62 #100DaysOfCode Create a Complex JSX Element
    React section of FreeCodeCamp

---
+++ day62b.js
@@ -0,0 +1,9 @@
+index.jsx const JSX =<div>
+  <h1>Ivy League Trio</h1>
+  <p>Three major rivals located in MA, CT, and NJ:</p>
+  <ul><li>Harvard</li>
+      <li>Yale</li>
+      <li>Princeton</li></ul></div>;
+
+// write your code here
+
