6dec607d81ed96c04f343b83fe919bd68a6b0551
cloudman <cloudman@mac.com>
2019-04-21 23:58:57 -0500

    day62 #100daysOfCode Write a React Component from Scratch
    React section of FreeCodeCamp

---
+++ day62.js
@@ -0,0 +1,14 @@
+index.jsx // change code below this line
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+            <h1>My First React Component!</h1>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
