111eb97210fc14341f0977bf721e4a76470f7bb0
cloudman <cloudman@mac.com>
2019-04-21 22:09:33 -0500

    day62 #100DaysOfCode Render HTML Elements to the DOM
    React section of FreeCodeCamp

---
+++ day62d.js
@@ -0,0 +1,8 @@
+index.jsx const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// change code below this line
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
