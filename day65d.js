c560882ac6cd3d9c370679437ef2240367741170
cloudman <cloudman@mac.com>
2019-04-24 23:28:29 -0500

    day65 #100DaysOfCode Pass State as Props to Child Component
    React section of FreeCodeCamp

---
+++ day65d.js
@@ -0,0 +1,28 @@
+index.jsx class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         <Navbar name = {this.state.name} />
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      <h1>Hello, my name is: {this.props.name} </h1>
+    </div>
+    );
+  }
+};
