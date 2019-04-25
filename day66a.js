cab2b9a6e5be8036e11f98b68a586d01332acc83
cloudman <cloudman@mac.com>
2019-04-25 15:28:08 -0500

    day66 #100DaysOfCode Use the Lifecycle Method componentWillMount
    React section of FreeCodeCamp

---
+++ day66a.js
@@ -0,0 +1,13 @@
+index.jsx class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // change code below this line
+  console.log("hello before mounting");
+    // change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
