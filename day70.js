2861cf7120dc1c86cce47d57b430794dfe1b0ca7
cloudman <cloudman@mac.com>
2019-04-29 23:57:18 -0500

    day70 #100DaysOfCode Render React on the Server with renderToString
    React section of FreeCodeCamp

---
+++ day70.js
@@ -0,0 +1,12 @@
+index.jsx 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+// change code below this line
+ReactDOMServer.renderToString(<App />);
